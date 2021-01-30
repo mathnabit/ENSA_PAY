52c1281c62fa1c96736ce969c185700579402766
omar atbd <omar.atbd@gmail.com>
2021-01-30 11:45:32 +0100

    document_admin

---
+++ src/main/java/com/ensapay/core/web/LoginAdminWS.java
@@ -0,0 +1,12 @@
+package com.ensapay.core.web;
+
+import org.springframework.stereotype.Component;
+
+import javax.xml.ws.WebServiceClient;
+
+@Component
+@WebServiceClient
+public class LoginAdminWS {
+
+}
+
