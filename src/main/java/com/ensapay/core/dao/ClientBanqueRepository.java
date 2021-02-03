bb85bc5a32c5325de9f7bcf76da1a18b75d48a24
omar atbd <omar.atbd@gmail.com>
2021-02-03 10:45:43 +0100

    demanderCompte rejeterDemande recupererDemandes creerCompteClient

--- src/main/java/com/ensapay/core/dao/ClientBanqueRepository.java
+++ src/main/java/com/ensapay/core/dao/ClientBanqueRepository.java
@@ -1,13 +1,10 @@
 package com.ensapay.core.dao;
 
 import com.ensapay.core.entities.Client;
+import com.ensapay.core.entities.ClientBanque;
 import com.ensapay.core.entities.Demande;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
-public interface DemandeRepository extends MongoRepository<Demande,String> {
-    List<Demande> findByResolu(boolean b);
-
-    Demande findByTel(String tel);
+public interface ClientBanqueRepository extends MongoRepository<ClientBanque,String> {
+    ClientBanque findByTel(String tel);
 }
--- src/main/java/com/ensapay/core/entities/Client.java
+++ src/main/java/com/ensapay/core/entities/Client.java
@@ -1,10 +1,24 @@
 package com.ensapay.core.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "clients")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Client {
 
     private String id;
     private String login;
     private String pass;
+    private String tel;
+    private String email;
+    private String type_cmpte;
 
 
 }
